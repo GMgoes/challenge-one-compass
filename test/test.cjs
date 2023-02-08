d2b944c5fc82c3e3e2b91869aeeadab1382c2b20
gusmgoes <ggoes269@gmail.com>
2023-02-08 14:56:22 -0300

    Elaboração do conjunto de testes chai/mocha

---
+++ test/test.cjs
@@ -0,0 +1,24 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable no-unused-vars */
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable no-undef */
+
+/* const chai = require('chai');
+
+const { expect } = chai.expect;
+const should = chai.should();
+const chaiHttp = require('chai-http');
+const rotas = require('../routes/routineEvents');
+
+chai.use(chaiHttp);
+
+describe('Testes', () => {
+  it('Teste de GET(TODOS) rotinas', async (done) => {
+    await chai.request(rotas)
+      .get('/events')
+      .end((err, response) => {
+        response.should.have.status(200);
+        done();
+      });
+  });
+}); */
